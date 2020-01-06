381bcd14855e4bf513bdaeab96458d880266aa3f
Thiago Moreira <thiago.gontijo@smn.com.br>
2020-01-06 18:02:56 -0300

    controllers consumer / sale

--- app/Controllers/Http/ConsumerController.js
+++ app/Controllers/Http/ConsumerController.js
@@ -1,6 +1,54 @@
 'use strict'
 
+const Consumer = use('App/Models/Consumer')
 class ConsumerController {
+  async index ({ params, request }) {
+    const { page = 1 } = request.get()
+    const consumers = await Consumer.query().paginate(page)
+    return consumers
+  }
+
+  async store ({ request }) {
+    const data = request.only([
+      'name',
+      'phone_number',
+      'address', 
+      'house_number',
+      'neighborhood',
+      'zip_code',
+      'state',
+      'country'
+    ])
+    const consumer = await Consumer.create(data)
+    return consumer
+  }
+
+  async show ({ params }) {
+    const consumer = await Consumer.findOrFail(params.id)
+    return consumer
+  }
+
+  async update ({ params, request }) {
+    const consumer = await Consumer.findOrFail(params.id)
+    const data = request.only([
+      'name',
+      'phone_number',
+      'address', 
+      'house_number',
+      'neighborhood',
+      'zip_code',
+      'state',
+      'country'
+    ])
+    consumer.merge(data)
+    await consumer.save()
+    return consumer
+  }
+
+  async destroy ({ params }) {
+    const consumer = await Consumer.findOrFail(params.id)
+    consumer.delete()
+  }
 }
 
 module.exports = ConsumerController
--- app/Controllers/Http/SaleController.js
+++ app/Controllers/Http/SaleController.js
@@ -1,6 +1,54 @@
 'use strict'
 
+const Sale = use('App/Models/Sale')
+
 class SaleController {
+  async index ({ params, request }) {
+    const { page = 1 } = request.get()
+    const sales = await Sale.query().paginate(page)
+    return sales
+  }
+
+  async store ({ request }) {
+    const data = request.only([
+      'tupperware_id',
+      'consumer_id',
+      'payment_method', 
+      'quantity_installments',
+      'description',
+      'consumer_name'
+    ])
+    const sale = await Sale.create(data)
+    return sale
+  }
+
+  async show ({ params }) {
+    const sale = await Sale.findOrFail(params.id)
+    return sale
+  }
+
+  async update ({ params, request }) {
+    const sale = await Sale.findOrFail(params.id)
+    const data = request.only([
+      'name',
+      'phone_number',
+      'address', 
+      'house_number',
+      'neighborhood',
+      'zip_code',
+      'state',
+      'country'
+    ])
+
+    sale.merge(data)
+    await sale.save()
+    return sale
+  }
+
+  async destroy ({ params }) {
+    const sale = await Sale.findOrFail(params.id)
+    sale.delete()
+  }
 }
 
 module.exports = SaleController
